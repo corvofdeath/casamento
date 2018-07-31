63ecbcc88f7880782af19b92cc1ba057c8c0df03
Filipe Santiago <filipe@upplify.com>
2018-07-31 14:21:33 -0300

    add tree view

--- view/src/main.js
+++ view/src/main.js
@@ -1,22 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import BootstrapVue from 'bootstrap-vue'
-import vueSmoothScroll from 'vue-smooth-scroll'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import BootstrapVue from 'bootstrap-vue';
+import vueSmoothScroll from 'vue-smooth-scroll';
+import VueTreeNavigation from 'vue-tree-navigation';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 library.add(fas)
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(BootstrapVue);
-Vue.use(vueSmoothScroll)
+Vue.use(vueSmoothScroll);
+Vue.use(VueTreeNavigation);
 
 Vue.config.productionTip = false
 
@@ -24,4 +27,4 @@ Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
