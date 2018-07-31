a9e02b20a4eba077c10b3cb12085301c0e37f255
Filipe Santiago <filipe@upplify.com>
2018-07-31 08:36:46 -0300

    add smooth scroll

--- view/src/main.js
+++ view/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import BootstrapVue from 'bootstrap-vue'
+import vueSmoothScroll from 'vue-smooth-scroll'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,6 +11,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
+Vue.use(vueSmoothScroll)
 
 new Vue({
   router,
