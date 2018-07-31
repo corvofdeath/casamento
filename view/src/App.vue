1b35958ace7550a6128c2cb38aab8d3d2cc52091
Filipe Santiago <filipe@upplify.com>
2018-07-31 14:18:25 -0300

    add default class for app css

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Pacifico');
 @import url('https://fonts.googleapis.com/css?family=Amatic+SC|Open+Sans');
+
+.padding10 {
+    padding: 10px;
+}
+
+.padding20 {
+    padding: 20px;
+}
+
+.center {
+    text-align: center;
+}
 </style>
