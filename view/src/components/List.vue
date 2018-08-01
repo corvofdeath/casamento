30f69e110072a803713e29d0c9443edc4edd0122
Filipe Santiago <filipe@upplify.com>
2018-08-01 08:43:43 -0300

    add list component

---
+++ view/src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="history">
+        <b-row class="title">
+            <b-col class="center">
+                <h1>Lista de Casamento</h1>
+            </b-col>
+        </b-row>
+        <div class="padding20"></div>
+        <b-row class="content">
+            <b-col class="center">
+                <p>Content</p>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "app-list"
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+    font-family: 'Amatic SC', cursive
+}
+
+.content {
+    font-family: 'Pacifico', cursive;
+}
+</style>
+
+
