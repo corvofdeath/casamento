2b5b092446d2c09c4f9d119368b223af6adac0db
Filipe Santiago <filipe@upplify.com>
2018-08-02 09:44:07 -0300

    add config for prod db

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 export default {
-    database: 'mongodb://localhost:27017/koa',
+    // database: 'mongodb://localhost:27017/koa',
+    database: 'mongodb://admin:batima91939@ds111082.mlab.com:11082/casamento',
     jwt: {
         secret: 't3st3S3cr3t3',
         session: false
