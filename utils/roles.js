236284b97a91ef058d9a0be527094ca3a2ae31d4
Filipe Santiago <filipe@upplify.com>
2018-08-09 10:39:51 -0300

    refator roles to utils

--- utils/roles.js
+++
@@ -1,7 +0,0 @@
-/**
- * Defines all the roles used by the system
- */
-export default {
-    user: 0,
-    client: 1
-}
---
+++ utils/types.js
@@ -0,0 +1,15 @@
+/**
+ * Defines all the roles used by the system
+ */
+export const roles = {
+    user: 0,
+    client: 1
+}
+
+/**
+ * Define familys
+ */
+export const family = {
+    fiance: "noivo",
+    bride: "noiva"
+}
