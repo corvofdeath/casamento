89abf20d54615ac97862f1c1e26af1afb94de94b
Filipe Santiago <filipe@upplify.com>
2018-08-09 10:20:01 -0300

    add roles to system

---
+++ models/account.model.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+import { genSalt, hash as _hash, compare } from 'bcrypt-nodejs';
+
+import roles from '../utils/roles';
+
+let accountSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    name: {
+        type: String,
+        required: true
+    }, 
+    role: {
+        type: String,
+        required: true,
+        default: roles.client
+    }
+});
+
+// Execute before each account.save() call
+accountSchema.pre('save', function (callback) {
+    let account = this;
+
+    // Break out if the password hasn't changed
+    if (!account.isModified('password')) return callback();
+
+    // Password changed so we need to hash it
+    genSalt(5, (err, salt) => {
+        if (err) return callback(err);
+
+        _hash(account.password, salt, null, (err, hash) => {
+            if (err) return callback(err);
+            account.password = hash;
+            callback();
+        });
+    });
+});
+
+accountSchema.methods.verifyPassword = (password, hash) => {
+    return new Promise((resolve, reject) => {
+        compare(password, hash, (err, isMatch) => {
+            if (err) reject(new Error(err));
+
+            resolve(isMatch);
+        })
+    })
+}
+
+export default mongoose.model('accounts', accountSchema);
---
+++ utils/roles.js
@@ -0,0 +1,7 @@
+/**
+ * Defines all the roles used by the system
+ */
+export default {
+    user: 0,
+    client: 1
+}
